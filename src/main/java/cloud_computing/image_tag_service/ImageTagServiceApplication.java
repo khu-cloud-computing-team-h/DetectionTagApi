f05fe35713b9036ba15f81380adae080ba6224f1
hyunwon <khw73850@gmail.com>
2024-05-23 16:44:25 +0900

    init

---
+++ src/main/java/cloud_computing/image_tag_service/ImageTagServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud_computing.image_tag_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageTagServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageTagServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud_computing/image_tag_service/ImageTagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud_computing.image_tag_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageTagServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
