677f578d836021f1fe9c7d5e1b68e1172b4fdafe
hyunwon <khw73850@gmail.com>
2024-06-12 21:04:12 +0900

    feat: api 수정(기존 태그 생성 해서 반환하는 api를 생성하는 api 와 조회하는 api를 분리)

--- src/main/java/cloud_computing/image_tag_service/controller/ImageTagDetectionController.java
+++ src/main/java/cloud_computing/image_tag_service/controller/ImageTagDetectionController.java
@@ -2,40 +2,35 @@ package cloud_computing.image_tag_service.controller;
 
 import cloud_computing.image_tag_service.dto.TagListResponse;
 import cloud_computing.image_tag_service.exception.ImageFileProcessingException;
-import cloud_computing.image_tag_service.service.TagDetectionService;
-import cloud_computing.image_tag_service.service.TranslationService;
-import com.google.protobuf.ByteString;
+import cloud_computing.image_tag_service.service.TagDetectionClient;
+import cloud_computing.image_tag_service.service.TagService;
+import cloud_computing.image_tag_service.service.TranslationClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/vision/tag")
+@RequestMapping("/api/vision/")
 @RequiredArgsConstructor
 public class ImageTagDetectionController {
-    private final TagDetectionService tagDetectionService;
-    private final TranslationService translationService;
-    @PostMapping("/detect")
-    public ResponseEntity<?> detectTagsFromImage(@RequestParam MultipartFile file){
-        List<String> englishTagList;
-
+    private final TagService tagService;
+    @PostMapping("/tags/detect")
+    public ResponseEntity<?> detectTagsFromImage(@RequestAttribute BigDecimal userId, @RequestParam MultipartFile imageFile){
         try{
-            englishTagList = tagDetectionService.detectTag(file.getInputStream());
+            tagService.createTags(userId, imageFile.getInputStream());
         }catch(IOException e){
             throw new ImageFileProcessingException("이미지 파일 처리 오류", e);
         }
 
-        List<String> koreanTagList = translationService.translate(englishTagList);
-
-        return ResponseEntity.ok(TagListResponse.builder().tags(koreanTagList).build());
+        return ResponseEntity.ok().build();
+    }
+    @GetMapping("/tags")
+    public ResponseEntity<?> getTagsFromUserId(@RequestAttribute BigDecimal userId){
+        return ResponseEntity.ok(TagListResponse.builder().tags(tagService.findTags(userId)).build());
     }
 }
